ee7e0a266b3fc2b676ef55b3e01da190bdf6ede3
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2022-01-07 20:46:20 -0500

    Update ApiResponse.java

--- GeoMusicSlashCommands/src/main/java/com/aliucord/plugins/geomusic/ApiResponse.java
+++ GeoMusicSlashCommands/src/main/java/com/aliucord/plugins/geomusic/ApiResponse.java
@@ -2,5 +2,5 @@ package com.aliucord.plugins.geomusic;
 
 // This class will be used to deserialize the json into a java Object. It matches the json returned by the API
 public class ApiResponse {
-    public String file;
+    public String uptime;
 }
